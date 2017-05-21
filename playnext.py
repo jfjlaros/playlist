00710b21e7c89117406a279d4b9ec67a7641d9ab
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2017-05-21 22:54:02 +0200

    Changed database structure.

--- playnext.py
+++ playnext.py
@@ -5,17 +5,6 @@ import sys
 import yaml
 
 
-def _get_files(path):
-    files = []
-
-    for file_name in os.listdir(path):
-        if not file_name.startswith('.'):
-            if os.path.isfile(os.path.join(path, file_name)):
-                files.append(file_name)
-
-    return files
-
-
 class PlayDB(object):
     def __init__(self):
         config_path = '{}/.cache/playnext'.format(os.getenv('HOME'))
@@ -27,62 +16,81 @@ class PlayDB(object):
 
         self._db = yaml.safe_load(open(self._config, 'r'))
 
+        self._path = os.path.realpath('.')
+        if self._path not in self._db:
+            self._db[self._path] = {'files': [], 'offset': 0}
+
+        self._item = self._db[self._path]
+
     def __del__(self):
         yaml.safe_dump(
             self._db, open(self._config, 'w'), width=76,
             default_flow_style=False)
 
-    def __contains__(self, key):
-        return os.path.realpath(key) in self._db
+    def _overflow(self):
+        return self._item['offset'] >= len(self._item['files'])
 
-    def __getitem__(self, key):
-        return self._db[os.path.realpath(key)] if key in self else -1
+    def add(self, files):
+        for file_name in files:
+            if not file_name.startswith('.') and os.path.isfile(file_name):
+                if file_name not in self._item['files']:
+                    self._item['files'].append(file_name)
 
-    def __setitem__(self, key, value):
-        self._db[os.path.realpath(key)] = value
+    def remove(self, files):
+        for index, file_name in enumerate(self._item['files']):
+            if file_name in files:
+                self._item['files'].pop(index)
+                if index < self._item['offset']:
+                    self._item['offset'] -= 1
 
-    def __delitem__(self, key):
-        if key in self:
-            del self._db[os.path.realpath(key)]
+    def set(self, filename):
+        for index, file_name in enumerate(self._item['files']):
+            if file_name == filename:
+                self._item['offset'] = index
+                return
 
-    def add(self, path):
-        self[path] = len(_get_files(path)) - 1
-
-    def remove(self, path):
-        del self[path]
-
-    def set(self, path, value):
-        self[path] = value
-
-    def current(self, path):
-        if self[path] < 0:
+    def current(self):
+        if self._overflow():
             return ''
-        return sorted(_get_files(path), reverse=True)[self[path]]
+        return self._item['files'][self._item['offset']]
 
-    def next(self, path):
-        self[path] = max(self[path] - 1, -1)
+    def next(self):
+        if self._overflow():
+            return
+        self._item['offset'] += 1
+
+    def show(self):
+        for index, file_name in enumerate(self._item['files']):
+            spacer = ' '
+            if index == self._item['offset']:
+                spacer = '*'
+            print '{} {}'.format(spacer, file_name)
 
 
-def add():
-    PlayDB().add('.')
+def add(files):
+    PlayDB().add(files)
 
 
-def remove():
-    PlayDB().remove('.')
+def remove(files):
+    PlayDB().remove(files)
 
 
-def set(value):
-    PlayDB().set('.', value)
+def set(filename):
+    PlayDB().set(filename)
 
 
 def current():
-    print PlayDB().current('.')
+    print PlayDB().current()
 
 
 def next():
     db = PlayDB()
-    print db.current('.')
-    db.next('.')
+    print db.current()
+    db.next()
+
+
+def show():
+    PlayDB().show()
 
 
 def main():
@@ -92,11 +100,26 @@ def main():
     #parser.add_argument('-v', action='version', version=version(parser.prog))
     subparsers = parser.add_subparsers(dest='subcommand')
 
-    subparsers.add_parser('add').set_defaults(func=add)
-    subparsers.add_parser('remove').set_defaults(func=remove)
-    subparsers.add_parser('set').set_defaults(func=set)
-    subparsers.add_parser('current').set_defaults(func=current)
-    subparsers.add_parser('next').set_defaults(func=next)
+    subparser = subparsers.add_parser('add')
+    subparser.add_argument('files', metavar='FILE', type=str, nargs='+')
+    subparser.set_defaults(func=add)
+
+    subparser = subparsers.add_parser('remove')
+    subparser.add_argument('files', metavar='FILE', type=str, nargs='+')
+    subparser.set_defaults(func=remove)
+
+    subparser = subparsers.add_parser('set')
+    subparser.add_argument('filename', metavar='FILE', type=str)
+    subparser.set_defaults(func=set)
+
+    subparser = subparsers.add_parser('current')
+    subparser.set_defaults(func=current)
+
+    subparser = subparsers.add_parser('next')
+    subparser.set_defaults(func=next)
+
+    subparser = subparsers.add_parser('show')
+    subparser.set_defaults(func=show)
 
     args = parser.parse_args()
 
