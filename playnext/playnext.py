57aa0565acedc8278abe568f499e4559a99b6f51
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2017-05-27 15:39:36 +0200

    Made package.

--- playnext/playnext.py
+++ playnext/playnext.py
@@ -1,6 +1,4 @@
-import argparse
 import os
-import sys
 
 import yaml
 
@@ -65,67 +63,3 @@ class PlayDB(object):
             if index == self._item['offset']:
                 spacer = '*'
             print '{} {}'.format(spacer, file_name)
-
-
-def add(files):
-    PlayDB().add(files)
-
-
-def remove(files):
-    PlayDB().remove(files)
-
-
-def set(filename):
-    PlayDB().set(filename)
-
-
-def current():
-    print PlayDB().current()
-
-
-def next():
-    db = PlayDB()
-    print db.current()
-    db.next()
-
-
-def show():
-    PlayDB().show()
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='', epilog='')
-    #parser.add_argument('-v', action='version', version=version(parser.prog))
-    subparsers = parser.add_subparsers(dest='subcommand')
-
-    subparser = subparsers.add_parser('add')
-    subparser.add_argument('files', metavar='FILE', type=str, nargs='+')
-    subparser.set_defaults(func=add)
-
-    subparser = subparsers.add_parser('remove')
-    subparser.add_argument('files', metavar='FILE', type=str, nargs='+')
-    subparser.set_defaults(func=remove)
-
-    subparser = subparsers.add_parser('set')
-    subparser.add_argument('filename', metavar='FILE', type=str)
-    subparser.set_defaults(func=set)
-
-    subparser = subparsers.add_parser('current')
-    subparser.set_defaults(func=current)
-
-    subparser = subparsers.add_parser('next')
-    subparser.set_defaults(func=next)
-
-    subparser = subparsers.add_parser('show')
-    subparser.set_defaults(func=show)
-
-    args = parser.parse_args()
-
-    args.func(**{k: v for k, v in vars(args).items()
-        if k not in ('func', 'subcommand')})
-
-
-if __name__ == '__main__':
-    main()
